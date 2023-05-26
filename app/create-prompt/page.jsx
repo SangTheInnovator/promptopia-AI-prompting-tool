19f919e5c76d06e2cc9fffb47a3a6a1e2f769eb1
Sang Le <phusang2501@gmail.com>
2023-05-27 02:18:25 +0300

    Authenticate Update

---
+++ app/create-prompt/page.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react';
+import { useState } from 'react';
+import { useSession } from 'next-auth/react';
+
+const CreatePrompt = () => {
+  return (
+    <div>CreatePrompt</div>
+  )
+}
+
+export default CreatePrompt
